22eb5008a075542899c6f76ddd35ae5a35b68e38
Thomas <thomasmongstad@gmail.com>
2024-12-21 14:51:42 +0100

    Testing different patterns based on MVT type

--- main.go
+++ main.go
@@ -6,7 +6,7 @@ import (
 	"regexp"
 	"strings"
 
-	mvthandler "github.com/mongstaen/avregex-go/messagehandlers/mvthandler"
+	"github.com/mongstaen/avregex-go/messagehandlers/mvthandler"
 )
 
 func processNewMessage(text string) {
@@ -19,7 +19,7 @@ func processNewMessage(text string) {
 	case "PDM":
 		processPDMMessage(text)
 	case "MVT":
-		mvthandler.ProcessMVTMessage(text)
+		mvthandler.NewProcessMVTMessage(text)
 	case "LDM":
 		fmt.Println("LDM")
 	case "COR":
@@ -43,14 +43,52 @@ func processCORMessage(text string) {
 	processNewMessage(newText)
 }
 func main() {
+	dep := `
+MVT
+SD200/21.PMDFG.CDG
+AD1100/1115 EA1500 FRA
+DL72/0015
+PX112
+SI DEICING
+`
+	arr := `
+MVT
+SD200/21.PMDFG.CDG
+AA1515/1520
+FLD22
+`
 	text := `
-COR
 MVT
 AA1203/07.LNREG.BGO
 AD1402/1413 EA1524 SVG
 PX123
 SI MAP1405
 `
+	mvt := `
+MVT
+AZ1074/08.HBXXX.ZRH
+AD1454/1458 EA1555 FRA
+DL46/02/0005/0004
+PX023
+DLA/02B//
+`
+	/* 	ldm := `
+	LDM
+	AZ464/08.HBXXX.C10Y92.2/3
+	-LCY.28/14/0/0.0.T262.1/262.PAX/1/41.JMP/0.CRW/0.PAD/0/0
+	SI LCY BAG 262 POS 0 FRE 0
+	PAX WEIGHTS USED M88 F70 C35 I0
+	SERVICE WEIGHT ADJUSTMENT WEIGHT/INDEX
+	ADD
+	NIL
+	DEDUCTIONS
+	NIL
+	LCY C 0 M 0 B 18/ 262 O 0 T 0
+	` */
 	//fmt.Println(text)
+	processNewMessage(dep)
+	processNewMessage(arr)
 	processNewMessage(text)
+	processNewMessage(mvt)
+	//processNewMessage(ldm)
 }
--- messagehandlers/mvthandler/mvthandler.go
+++ messagehandlers/mvthandler/mvthandler.go
@@ -40,6 +40,40 @@ func handleMVTType(mvtType string) {
 	// TODO make functionality to handle AD, EA, AA messages
 }
 
+func findMVTType(text string) string {
+	text = strings.TrimSpace(text)
+	pattern := `(?m)^(?:.*\n){2}([A-Z]{2})`
+	regex := regexp.MustCompile(pattern)
+	mvtType := regex.FindStringSubmatch(text)[1]
+	return mvtType
+}
+
+func newHandleMVTType(mvtType string) string {
+	pattern := ""
+	switch mvtType {
+	case "AD":
+		log.Println("Handled AD Movement Type")
+		pattern = `(?i)([A-Z]{3})\n([A-Z0-9]{3,})\/([0-9]{2})\.([A-Z0-9]{5,}).([A-Z]{3})\n([A-Z]{2})([0-9]{4,6})\/([0-9]{4,6}) [A-Z]{2}([0-9]{4}) ([A-Z]{3})\n((?s).*)`
+	case "ED":
+		log.Println("Handled ED Movement Type")
+		pattern = "ED"
+	case "AA":
+		log.Println("Handled AA Movement Type")
+		pattern = `(?i)([A-Z]{3})\n([A-Z0-9]{3,})\/([0-9]{2})\.([A-Z0-9]{5,}).([A-Z]{3})\n([A-Z]{2})([0-9]{4,6})\/([0-9]{4,6})\n((?s).*)`
+	case "EA":
+		log.Println("Handled EA Movement Type")
+		pattern = "EA"
+	case "EO":
+		log.Println("Handled EO Movement Type")
+		pattern = "EO"
+	default:
+		log.Println("Unknown Movement Type")
+	}
+
+	return pattern
+	// TODO make functionality to handle AD, EA, AA messages
+}
+
 func ProcessMVTMessage(text string) {
 	text = strings.TrimSpace(text)
 	pattern := `(?i)([A-Z]{3})\n([A-Z0-9]{3,})\/([0-9]{2})\.([A-Z0-9]{5,}).([A-Z]{3})\n([A-Z]{2})([0-9]{4,6})\/([0-9]{4,6}) [A-Z]{2}([0-9]{4}) ([A-Z]{3})\n((?s).*)`
@@ -75,5 +109,46 @@ func ProcessMVTMessage(text string) {
 	fmt.Println("SI: \n", message.messageSi)
 
 	// TODO: Make UUID for message, and store entire message in DB
+	// TODO: IE: UUID, Unique Flight ID, Message
+
+	// TODO: Once flights DB, find flight and update required fields
+}
+
+func NewProcessMVTMessage(text string) {
+	mvtType := findMVTType(text)
+	pattern := newHandleMVTType(mvtType)
+	regex := regexp.MustCompile(pattern)
+	result := regex.FindStringSubmatch(text)
+
+	fmt.Println(result[4])
+	/* 	message := Message{
+	   		messageType:          result[1],
+	   		flightNumber:         result[2],
+	   		flightDate:           result[3],
+	   		aircraftRegistration: result[4],
+	   		flightOrigin:         result[5],
+	   		mvtType:              result[6],
+	   		pushTime:             result[7],
+	   		takeoffTime:          result[8],
+	   		etaTime:              result[9],
+	   		flightDestination:    result[10],
+	   		messageSi:            result[11],
+	   	}
+
+	   	fmt.Println("Message Type: \t\t", message.messageType)
+	   	fmt.Println("Flight Number: \t\t", message.flightNumber)
+	   	fmt.Println("Flight Date: \t\t", message.flightDate)
+	   	fmt.Println("Aircraft Registration: \t", message.aircraftRegistration)
+	   	fmt.Println("Flight Origin: \t\t", message.flightOrigin)
+	   	fmt.Println("Flight Destination: \t", message.flightDestination)
+	   	fmt.Println("MVT Type: \t\t", message.mvtType)
+	   	fmt.Println("Push Time: \t\t", message.pushTime)
+	   	fmt.Println("Takeoff Time: \t\t", message.takeoffTime)
+	   	fmt.Println("ETA Time: \t\t", message.etaTime)
+	   	fmt.Println("SI: \n", message.messageSi) */
+
+	// TODO: Make UUID for message, and store entire message in DB
+	// TODO: IE: UUID, Unique Flight ID, Message
+
 	// TODO: Once flights DB, find flight and update required fields
 }
