58e70055b4ebfd722ceb42a51f417678df6fd2d0
Thomas <thomasmongstad@gmail.com>
2024-12-28 15:12:38 +0100

    works better like this.. i think

--- main.go
+++ main.go
@@ -30,48 +30,79 @@ func processNewMessage(text string) {
 func processMVTMessage(text string) {
 	text = strings.TrimSpace(text)
 	lines := strings.Split(text, "\n")
+
 	fmt.Println(text)
+
+	// Handle line 2
+	line2Parts := regexp.MustCompile(`([A-Z0-9]{3,})\/([0-9]{2})\.([A-Z0-9]{5,}).([A-Z]{3})`).FindStringSubmatch(lines[1])
+	flightNumber, flightDayOfMonth, aircraftRegistration, flightOrigin := line2Parts[1], line2Parts[2], line2Parts[3], line2Parts[4]
+	fmt.Printf("Flight Number: %v\nFlight Day: %v\nAircraft Registration: %v\nFlight Origin: %v\n", flightNumber, flightDayOfMonth, aircraftRegistration, flightOrigin)
 
 	// Handle line 3
 	parts := strings.Split(lines[2], " ")
-
 	for _, part := range parts {
-		//fmt.Println(part)
 		if strings.HasPrefix(part, "ED") {
-			fmt.Println("Part starts with ED:", part)
+			part := strings.TrimPrefix(part, "ED")
+			fmt.Println("Estimated Departure:", part)
 		}
 		if strings.HasPrefix(part, "AD") {
-			fmt.Println("Part starts with AD:", part)
+			part := strings.TrimPrefix(part, "AD")
+			offBlockTime, takeOffTime := strings.Split(part, "/")[0], strings.Split(part, "/")[1]
+			fmt.Printf("Off Block: %v, Takeoff: %v\n", offBlockTime, takeOffTime)
 		}
 		if strings.HasPrefix(part, "EA") {
-			fmt.Println("Part starts with EA:", part)
+			part := strings.TrimPrefix(part, "EA")
+			fmt.Println("Estimated Arrival:", part)
 		}
 		if strings.HasPrefix(part, "AA") {
-			fmt.Println("Part starts with AA:", part)
+			part := strings.TrimPrefix(part, "AA")
+			touchDownTime, onBlockTime := strings.Split(part, "/")[0], strings.Split(part, "/")[1]
+			fmt.Printf("Touchdown: %v, On Block: %v\n", touchDownTime, onBlockTime)
 		}
 		if strings.HasPrefix(part, "EO") {
 			fmt.Println("Part starts with EO:", part)
 		}
 		if strings.HasPrefix(part, "NI") {
-			fmt.Println("Part starts with NI:", part)
+			part := strings.TrimPrefix(part, "NI")
+			fmt.Println("New Information at:", part)
 		}
 		if len(part) == 3 {
 			fmt.Println("Flight Destination:", part)
 		}
 	}
 
+	// Handle line 4 - end
+	if strings.HasPrefix(lines[3], "DL") {
+		delayParts := strings.Split(lines[3], "/")
+
+		if len(delayParts) == 2 {
+			delayReason1 := strings.TrimPrefix(delayParts[0], "DL")
+			delayDuration1 := delayParts[1]
+			fmt.Printf("Delay Reason: %v, Duration: %v\n", delayReason1, delayDuration1)
+		}
+		if len(delayParts) == 4 {
+			delayReason1 := strings.TrimPrefix(delayParts[0], "DL")
+			delayReason2 := delayParts[1]
+			delayDuration1 := delayParts[2]
+			delayDuration2 := delayParts[3]
+			fmt.Printf("Delay Reason: %v, Duration: %v\n", delayReason1, delayDuration1)
+			fmt.Printf("Delay Reason: %v, Duration: %v\n", delayReason2, delayDuration2)
+		}
+
+	}
 	for _, line := range lines[3:] {
 		if strings.HasPrefix(line, "PX") {
 			numberOfPassengers := strings.TrimPrefix(line, "PX")
-			fmt.Println("PX is present:", numberOfPassengers)
+			numberOfPassengers = strings.TrimPrefix(numberOfPassengers, "0")
+			fmt.Println("Pax:", numberOfPassengers)
+		}
+
+		if strings.HasPrefix(line, "SI") {
+			supplementaryInformation := strings.TrimPrefix(line, "SI ")
+			fmt.Println("SI:", supplementaryInformation)
 		}
 	}
-	if strings.HasPrefix(lines[3], "DL") {
-		fmt.Println("DL is present")
-		// TODO Handle DL..
-	}
-
-	fmt.Println("\n")
+	fmt.Println("")
 
 }
 
